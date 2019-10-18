843687b5da2d0a37db60b3b2ff22b40f016dac57
iriszero <31085979+iriszero48@users.noreply.github.com>
2019-10-18 17:22:47 +0800

    Create HttpAutoIndexServer(g++).sh

---
+++ HttpAutoIndexServer(g++).sh
@@ -0,0 +1,2 @@
+git clone https://github.com/iriszero48/HttpAutoIndexServer.git
+g++ HttpAutoIndexServer/HttpAutoIndexServer/main.cpp -o HttpAutoIndexServer/HttpAutoIndexServer.out -std=c++17 -pthread
