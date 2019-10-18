b661fa133a6685628f653cf4da82d45ba5bb8256
iriszero <31085979+iriszero48@users.noreply.github.com>
2019-10-18 17:23:44 +0800

    Delete HttpAutoIndexServer(g++).sh

--- HttpAutoIndexServer(g++).sh
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/iriszero48/HttpAutoIndexServer.git
-g++ HttpAutoIndexServer/HttpAutoIndexServer/main.cpp -o HttpAutoIndexServer/HttpAutoIndexServer.out -std=c++17 -pthread
