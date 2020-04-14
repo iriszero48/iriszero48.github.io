438a37a7e9ad4e88319346e4e7b75c95cec56138
iriszero <31085979+iriszero48@users.noreply.github.com>
2020-04-14 17:32:01 +0800

    Update HttpAutoIndexServer.sh

--- HttpAutoIndexServer.sh
+++ HttpAutoIndexServer.sh
@@ -5,6 +5,6 @@
 ## by iriszero
 
 git clone https://github.com/iriszero48/HttpAutoIndexServer.git
-g++ -o HttpAutoIndexServer/HttpAutoIndexServer.out -O3 -Wl,--no-undefined -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack HttpAutoIndexServer/HttpAutoIndexServer/main.cpp -pthread -std=c++17
+g++ -o HttpAutoIndexServer/HttpAutoIndexServer.out -O2 -Wl,--no-undefined -Wl,-z,relro -Wl,-z,now -Wl,-z,noexecstack HttpAutoIndexServer/HttpAutoIndexServer/main.cpp -pthread -std=c++17
 test -f HttpAutoIndexServer/HttpAutoIndexServer.out && r=true || r=false
 echo g++ $r
