94d2145deff3f90ddbee4f54e30a079e39159cee
iriszero <31085979+iriszero48@users.noreply.github.com>
2020-08-04 13:42:25 +0800

    Create FileMd5Database.sh

---
+++ FileMd5Database.sh
@@ -0,0 +1,36 @@
+threads=`cat /proc/cpuinfo | grep "processor" | wc -l`
+
+source /etc/os-release
+
+if [[ "$ID" == "ubuntu" ]]; then
+
+    if [[ $VERSION_ID =~ (16|18) ]]; then
+        sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+        sudo apt update
+        sudo apt install g++-9 git cmake libboost-serialization-dev libboost-locale-dev libboost-filesystem-dev libboost-system-dev -y
+        git clone https://github.com/iriszero48/FileMd5Database.git
+        cd FileMd5Database && cmake FileMd5Database -DCMAKE_CXX_COMPILER=`where g++-9` && make -j $threads
+    elif [[ $VERSION_ID =~ (19|20) ]]; then
+        sudo apt update
+        sudo apt install g++ git cmake libboost-serialization-dev libboost-locale-dev libboost-filesystem-dev libboost-system-dev -y
+        git clone https://github.com/iriszero48/FileMd5Database.git
+        cd FileMd5Database && cmake FileMd5Database && make -j $threads
+    fi
+
+elif [[ "$ID" == "debian" || "$ID" == "raspbian" ]]; then
+
+    sudo apt update
+    sudo apt install g++ git cmake libboost-serialization-dev libboost-locale-dev libboost-filesystem-dev libboost-system-dev -y
+    git clone https://github.com/iriszero48/FileMd5Database.git
+    cd FileMd5Database && cmake FileMd5Database && make -j $threads
+
+elif [[ "$ID" == "gentoo" ]]; then
+
+    sudo sh -c "echo 'dev-libs/boost static-libs' >> /etc/portage/package.use/boost"
+    sudo emerge --ask --verbose dev-vcs/git dev-util/cmake dev-libs/boost
+    git clone https://github.com/iriszero48/FileMd5Database.git
+    cd FileMd5Database && cmake FileMd5Database && make -j $threads
+
+else
+    echo unsupport $ID
+fi
