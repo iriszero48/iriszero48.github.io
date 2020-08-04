d95bda9d714ee9509ff9dfc6d073a0ad79b1269c
iriszero <31085979+iriszero48@users.noreply.github.com>
2020-08-04 18:43:23 +0800

    Update FileMd5Database.sh

--- FileMd5Database.sh
+++ FileMd5Database.sh
@@ -13,7 +13,7 @@ if [[ "$ID" == "ubuntu" && $VERSION_ID =~ (16|18) ]]; then
     sudo apt update
     sudo apt install g++-9 git cmake libboost-serialization-dev libboost-locale-dev libboost-filesystem-dev libboost-system-dev -y
     git clone https://github.com/iriszero48/FileMd5Database.git
-    cd FileMd5Database && cmake FileMd5Database -DCMAKE_CXX_COMPILER=`where g++-9` && make -j $threads
+    cd FileMd5Database && cmake FileMd5Database -DCMAKE_CXX_COMPILER=`which g++-9` && make -j $threads
 elif [[ "$ID" == "ubuntu" && $VERSION_ID =~ (19|20) ]]; then
     sudo apt update
     sudo apt install g++ git cmake libboost-serialization-dev libboost-locale-dev libboost-filesystem-dev libboost-system-dev -y
